42c252cfba92633201a81271ab92f0810997b773
Lisa Jones <ljones4521@yahoo.com>
2016-06-26 23:15:23 -0400

    commit

--- currency.rb
+++ currency.rb
@@ -4,11 +4,29 @@ class Currency
    #currency_one.amount
   attr_reader :amount, :currency_symb
 
-  def initialize (currency_symb = "", amount)
-    @currency_symb = currency_symb
-    @amount = amount
-    if currency_symb != ""
+  def initialize (currency_symb = "", amount) #Did user provide the symbol
+     if currency_symb != ""  #was information passed
+      @amount = amount
+      @currency_symb = currency_symb
+    else
+       @currency_amount = amount[0]
+       puts @currency_amount
     end
+
+  end
+
+  def currency_symb_check
+    if @currency_symb_chk == "$"
+       @currency_symb_chk = :USD
+    elsif @currency_symb_check == "€"
+      @currency_symb_check = :EUR
+    end
+  end
+
+  def rem_symb # remove symbols present in the amount variable and assign to a country_code
+    rem_symb = @amount[1..-1].to_i
+    @currency_symb = rem_sym #assign rem_symb to the @currency instance variable so that I can assign to the correct country
+    currency_symb_check #I am attempting to run the method currency_symb_check
 
   end
 
@@ -19,8 +37,58 @@ class Currency
      false
     end
   end
-  
-  def Different_Current_Code
+
+  def Different_Current_Code #ERROR MESSAGE d  by using the .require_relative followed by the name of the  file.  It
+                            # imports the other file code so you have access to it.
 
     raise UnknownCurrencyCodeError
   end
+
+  def +(currency_object) #ADDITION.  We are saying use this method to
+                          #add the amount you pass to the amount prevoiusly passed and/or that is held
+                            #memory as amount...see initialize when we say @amount = amount
+                            #http://strugglingwithruby.blogspot.com/2010/04/operator-overloading.html
+
+      if  @currency_symb == currency_object.currency_symb
+        Currency.new(@amount +currency_object.amount)
+      else
+        raise UnknownCurrencyCodeError
+      end
+
+      #puts "The value of the new curent currecy is #{@amount}." #This will print the new amount.
+      #puts "The value of the currency_object's amout is #{currency_object.amount}"
+
+  end
+
+  def -(currency_object) #SUBTRACTION - We are saying use this method to
+                          #add the amount you pass to the amount prevoiusly passed and/or that is held
+                            #memory as amount...see initialize when we say @amount = amount
+                            #http://strugglingwithruby.blogspot.com/2010/04/operator-overloading.html
+
+      if  @currency_symb == currency_object.currency_symb
+        Currency.new(@amount -currency_object.amount)
+      else
+        raise UnknownCurrencyCodeError
+      end
+  end
+
+  def *(currency_object) #MULTIPLICATION  We are saying use this method to
+                          #add the amount you pass to the amount prevoiusly passed and/or that is held
+                            #memory as amount...see initialize when we say @amount = amount
+                            #http://strugglingwithruby.blogspot.com/2010/04/operator-overloading.html
+
+      if  @currency_symb == currency_object.currency_symb && currency_object.value.is_a?Float
+        true
+         Currency.new(@amount *currency_object.amount)
+      elsif
+        @currency_symb == currency_object.currency_symb && currency_object.value.is_a?Fixnum
+        false
+        Currency.new(@amount *currency_object.amount)
+      else
+        raise UnknownCurrencyCodeError
+      end
+  end
+
+end
+
+ currency_one = Currency.new.rem_symbol ($532)
---
+++ error.rb
@@ -0,0 +1,3 @@
+class UnknownCurrencyCodeError < StandardError
+
+end
