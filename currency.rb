01bd18aa6022fabc295285f0c51821358931a66e
Lisa Jones <ljones4521@yahoo.com>
2016-07-05 13:54:17 -0400

    Added currency conversion forumla

--- currency.rb
+++ currency.rb
@@ -4,28 +4,29 @@ class Currency
    #currency_one.amount
   attr_reader :amount, :currency_symb
 
-  def initialize (currency_symb = " ", amount) #Did user provide the symbol
-    if currency_symb != " "  #was information passed
-      @amount = amount  #initializing the instance variable
-      @currency_symb = currency_symb #assigns the symbol if something was passed
+
+  def initialize (currency_symb = " ", amount)
+     if currency_symb != " "
+      @amount = amount
+      @currency_symb = currency_symb
       currency_symb_check
     else
       @currency_symb = amount[0]
-      @amount = [1..-1].to_i#amount[0]
+      @amount = amount[1..-1].to_i #amount[0]
       currency_symb_check
     end
 
   end
 
-  def currency_symb_check #assign a currency symbol to its correct country
+  def currency_symb_check
     if @currency_symb == "$"
        @currency_symb = :USD
-    elsif @urrency_symb == "€"
+    elsif @currency_symb == "€"
       @currency_symb = :EUR
     end
   end
 
-  def ==(is_equal) #Compare two of the class objects to each other
+  def ==(is_equal)
     if @amount == is_equal.amount && @currency_symb == is_equal.currency_symb
       true
     else
@@ -33,46 +34,38 @@ class Currency
     end
   end
 
-  def Different_Current_Code #ERROR MESSAGE d  by using the .require_relative followed by the name of the  file.  It
-                            # imports the other file code so you have access to it.
-
+  def Different_Current_Code
     raise UnknownCurrencyCodeError
   end
 
-  def +(currency_object) #ADDITION.  We are saying use this method to
-                          #add the amount you pass to the amount prevoiusly passed and/or that is held
-                            #memory as amount...see initialize when we say @amount = amount
-                            #http://strugglingwithruby.blogspot.com/2010/04/operator-overloading.html
-
-      if  @currency_symb == currency_object.currency_symb
+#ADDITION.  We are saying use this method to add the amount you pass to the amount prevoiusly passed and/or that is held
+#memory as amount...see initialize when we say @amount = amount http://strugglingwithruby.blogspot.com/2010/04/operator-overloading.html
+  def +(currency_object)
+    if  @currency_symb == currency_object.currency_symb
         Currency.new(@amount +currency_object.amount)
-      else
+    else
         raise UnknownCurrencyCodeError
-      end
-
-      #puts "The value of the new curent currecy is #{@amount}." #This will print the new amount.
-      #puts "The value of the currency_object's amout is #{currency_object.amount}"
-
   end
+    #puts "The value of the new curent currecy is #{@amount}." #This will print the new amount.
+    #puts "The value of the currency_object's amout is #{currency_object.amount}"
+end
 
   def -(currency_object) #SUBTRACTION - We are saying use this method to
                           #add the amount you pass to the amount prevoiusly passed and/or that is held
                             #memory as amount...see initialize when we say @amount = amount
                             #http://strugglingwithruby.blogspot.com/2010/04/operator-overloading.html
 
-      if  @currency_symb == currency_object.currency_symb
+    if  @currency_symb == currency_object.currency_symb
         Currency.new(@amount -currency_object.amount)
       else
         raise UnknownCurrencyCodeError
       end
   end
 
-  def *(currency_object) #MULTIPLICATION  We are saying use this method to
-                          #add the amount you pass to the amount prevoiusly passed and/or that is held
-                            #memory as amount...see initialize when we say @amount = amount
-                            #http://strugglingwithruby.blogspot.com/2010/04/operator-overloading.html
-
-      if  @currency_symb == currency_object.currency_symb
+#MULTIPLICATION  We are saying use this method to add the amount you pass to the amount prevoiusly passed and/or that is held
+#memory as amount...see initialize when we say @amount = amount
+  def *(currency_object)
+    if  @currency_symb == currency_object.currency_symb
          Currency.new(@amount *currency_object.amount)
       elsif
         @currency_symb == currency_object.currency_symb
@@ -84,6 +77,6 @@ class Currency
 
 end
 
- currency_one = Currency.new("$", 532)
- puts currency_one.amount
- puts currency_one.currency_symb
+ # currency_one = Currency.new("$", 532)
+ # puts currency_one.amount
+ # puts currency_one.currency_symb
--- currency_converter.rb
+++ currency_converter.rb
@@ -1,21 +1,54 @@
-#require_relative "currency.rb" #!/usr/bin/env ruby -wKU
+require_relative 'error'
+require_relative 'currency.rb'
 
 class CurrencyConverter
 
-  def initialize (currency_hash = {})
-   @currency_hash = currency_hash
-   puts @currency_hash
- end
+  # attr_reader :currency_hash
 
+  def initialize (currency_hash = {})
+    @currency_hash = currency_hash
+  end
+
+
+  # this method takes in the currency code (eg USD)
+  # and gives back the currency sign for that code (eg $)
+  # we need this to be able to make a new currency object when
+  # the conversion takes place.
+  def currency_code_to_sign (currency_code)
+    if currency_code == :USD
+      return "$"
+    elsif currency_code == :EUR
+      return "€"
+    end
+  end
+
+
+  def convert (currency, currency_code)
+    if @currency_hash.has_key?(currency_code) #you may call this method on any hash
+
+      converted_amount = currency.amount * (@currency_hash[currency_code]/@currency_hash[currency.currency_symb])
+      new_currency_sign = currency_code_to_sign(currency_code)
+      converted_currency = Currency.new(new_currency_sign, converted_amount)
+    else
+      raise UnknownCurrencyCodeError
+    end
+  end
 
 end
 
- def convert (currency, currency_code)
-   if @currency_hash.has_key?(currency_code)
-     # do the convesion
-   else
-     raise UnknownCurrencyCodeError
-   end
- end
+#make ten_dollars
+ten_dollars = Currency.new("$10")
 
- 
+#intiliaze the converter object
+converter = CurrencyConverter.new({USD:1.00, EUR:0.90})
+
+#make the converter convert dollars to euros and return back a Currency obj
+ten_dollars_to_euros = converter.convert(ten_dollars, :EUR)
+
+
+#ten_euros = Currency.new("€", 10)
+# puts "#{ten_euros.amount} #{ten_euros.currency_symb}"
+
+#ten_euros_to_dollars = converter.convert(ten_euros, :USD)
+
+#puts ten_euros_to_dollars.amount
